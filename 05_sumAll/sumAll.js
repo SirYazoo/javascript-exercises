48ee026200c3cf1a65ff61e6ccb76e6ab015c851
SirYazoo <30350331+SirYazoo@users.noreply.github.com>
2025-01-19 22:42:45 +0700

    Solve exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    let finalSum = 0;
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+    if (start > end) {
+        const temp = start;
+        start = end;
+        end = temp;
+    }
+    for (let i = start; i <= end; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
