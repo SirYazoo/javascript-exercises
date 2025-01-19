241bb48f5a691a25d2cb58eac5e8ddc23919797a
SirYazoo <30350331+SirYazoo@users.noreply.github.com>
2025-01-19 22:57:45 +0700

    Solve exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let temp = (degree - 32) * 5 / 9;
+  return Math.round(temp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let temp = degree * 9 / 5 + 32;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
